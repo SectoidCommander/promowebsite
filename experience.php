c2e362ea5f08bc7f0dc9e1404f9a9197e0c2db07
SectoidCommander <ufh85@hotmail.com>
2017-03-12 18:07:26 +0000

    Created Experience and Project pages

---
+++ experience.php
@@ -0,0 +1,75 @@
+<?php
+    include("menu.php");
+?>
+<!DOCTYPE html>
+<head>
+
+</head>
+<body>
+    <div class="bodyPanel">
+    	<div class="content">
+	      	<h1>Experience</h1>
+	      	<section id="education">
+	      		<h3>Education</h3>
+	      			<article>
+	      			<div>University of Akureyri, Iceland</div>
+	      			<div>Bachelor's degree, Computer Science 2010</div>
+	      		</article>
+	      	</section>
+
+	      	<section id="skills">
+	      		<h3>Skills applied over the last three months</h3>
+	      			<article>
+	      			<div><b>Languages, frameworks and libraries</b></div>
+	      			<div>Java, PHP, Javascript, JQuery, MySQL, Spring, HTML, HTML5, CSS
+	      			<br/>
+	      			<br/>
+	      			<div><b>Editors, supporting tools</b></div>
+	      			<div>SVN version control, Jira, Eclipse IDE, Spring Roo, Composer 
+	      		</article>
+	      	</section>
+
+	      	<section id="employers">
+	      		<h3>Employers</h3>
+		      	<table class="table table-bordered myExperience">
+		      		<tr>
+		      			<td>
+		      				<a href="https://www.rannis.is/" target="_blank"><img src="images/rannis_logo.jpg" /></a>
+		      			</td>
+		      			<td>
+		      				<article>
+		      					<h4>RANNÍS - Icelandic Center of Research</h4>
+		      					<p>
+		      						The Icelandic Center for Research (RANNIS) supports research, innovation, education and culture in Iceland. The institution administers competitive funds in the fields of research, innovation, education and culture in addition to strategic research programmes. It also promotes public awareness of the aformentioned fields in Iceland.
+		      					</p>
+		      					<span class="date">
+		      						2012-05-02 to current
+		      					</span>
+		      				</article>
+		      			</td>
+		      		</tr>
+		      		<tr>
+		      			<td>
+		      				<a href="http://www.rikisendurskodun.is/en/" target="_blank"><img src="images/inao.png" /></a>
+		      			</td>
+		      			<td>
+		      				<article>
+		      					<h4>The Icelandic National Audit Office</h4>
+		      					<p>
+		      						An independent public institution whos role is to audit the state accounts and the financial statements of state bodies. In adittion it monitors and promotes improvements in the financial management of the state and in the use of public funds.
+		      					</p>
+		      					<span class="date">
+		      						2011-05-01 to 2012-06-01
+		      					</span>
+		      				</article>
+		      			</td>
+		      		</tr>
+		      	</table>
+		    </section>
+		</div>
+    </div>
+</body>
+
+<?php
+  include("footer.php");
+?>
