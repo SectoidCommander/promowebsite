443c2cb4da0c240d3f6f342ad2d48d8eec3ac3d4
SectoidCommander <ufh85@hotmail.com>
2017-03-12 18:06:35 +0000

    Created Experience and Project pages

--- portfolio.php
+++ portfolio.php
@@ -7,13 +7,15 @@
 </head>
 <body>
     <div class="bodyPanel">
-      <h1>Portfolio</h1>
-      <br/>
-      <p>Portfolio here</p>
-    
+    	<div class="content">
+	      	<h1>Portfolio</h1>
+		      	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed orci massa. Phasellus porttitor ex ante, ut volutpat ex efficitur quis. Etiam velit ex, varius sit amet aliquet et, volutpat id dolor. Duis finibus ligula et dui ullamcorper, nec fermentum massa imperdiet. Suspendisse dictum eu mauris in maximus. Sed ullamcorper diam eget diam dignissim, vitae efficitur tortor faucibus. Aliquam molestie nunc vel nulla tempor, vel pharetra ligula euismod. Donec porta fringilla ipsum ut eleifend. In orci nisl, congue at elit quis, euismod consectetur tellus. Phasellus suscipit nisl sit amet sollicitudin luctus. Cras ac justo non ante pulvinar malesuada sit amet non odio. Donec in auctor turpis. Quisque mollis odio eleifend, suscipit nisi finibus, viverra urna. Aliquam facilisis, metus at condimentum maximus, felis ligula varius erat, sed dignissim urna ante sit amet nisl. Ut auctor purus sed vulputate condimentum. Proin porttitor metus nec auctor porta.</p>
+
+				<p>Quisque a bibendum leo. Fusce ac sem convallis, rutrum urna ut, lacinia dui. Etiam volutpat enim a tincidunt molestie. Morbi pretium efficitur urna, nec consectetur justo porta et. Suspendisse aliquam magna vitae lorem sollicitudin suscipit. Aenean commodo urna et fringilla viverra. Donec ac velit eget massa porta mattis nec ut risus. Nunc ut lectus at quam ornare aliquet at non lectus. Sed in velit nec arcu ornare faucibus non nec libero. Etiam quis finibus nisi. Nunc nec viverra libero. Quisque urna eros, suscipit non lacus elementum, feugiat vehicula libero. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
+			</section>
+		</div>
     </div>
 </body>
-    </div>
 
 <?php
   include("footer.php");
